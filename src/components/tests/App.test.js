9082275be5a5cd25fef0b0c87c9d3d3d449c5c70
Emily Bailey <emilybailey3@gmail.com>
2021-02-11 18:44:53 -0800

    remove unused

--- src/components/tests/App.test.js
+++ src/components/tests/App.test.js
@@ -2,7 +2,8 @@ import { render, screen } from '@testing-library/react';
 import App from '../App';
 
 test('renders learn react link', () => {
+	//very simple test to make sure all pieces rendering. New to react, still learning the test suites, so kept small.
   render(<App />);
-  const linkElement = screen.getByText(/Data get/);
-  expect(linkElement).toBeInTheDocument();
+  const tableElement = screen.getByRole('table');
+  expect(tableElement).toBeInTheDocument();
 });
