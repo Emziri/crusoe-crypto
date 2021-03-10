2ac85a5c872f138586319b652f92119474adb8c7
Emily Bailey <emilybailey3@gmail.com>
2021-03-09 21:51:52 -0800

    styling tweaks, code cleanup

--- src/components/tests/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from '../App';
-
-test('renders learn react link', () => {
-	//very simple test to make sure all pieces rendering. New to react, still learning the test suites, so kept small.
-  render(<App />);
-  const tableElement = screen.getByRole('table');
-  expect(tableElement).toBeInTheDocument();
-});
